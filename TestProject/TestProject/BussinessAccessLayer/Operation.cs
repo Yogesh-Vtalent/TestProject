91e51cae30b156d46fe69ef84161cd11a78946aa
Yogesh-Vtalent <yogesh.vtalent@gmail.com>
2019-12-19 17:43:24 +0530

    Addition Method in BussinessAccessLayer Folder Added

---
+++ TestProject/TestProject/BussinessAccessLayer/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject.BussinessAccessLayer
+{
+    public class Operation
+    {
+        public int Addition()
+        {
+            int a=3, b=2;
+            int res = a + b;
+            return res;
+        }
+    }
+}
